fcf1ec9088cb89396bdaebfd8ae1c4827803592c
Prabh <iprabhjeet9@gmail.com>
2020-12-18 13:41:18 +0530

    Intial commit

---
+++ src/main/java/org/entropy/rest/webservices/ResWebServiceApplication.java
@@ -0,0 +1,13 @@
+package org.entropy.rest.webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/entropy/rest/webservices/ResWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.entropy.rest.webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

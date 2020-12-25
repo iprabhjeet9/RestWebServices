7a2b31e8ecc9827f671ad7837a7fb4569807aa65
Prabh <iprabhjeet9@gmail.com>
2020-12-25 18:42:49 +0530

    Added Hateos support

--- src/main/java/org/entropy/rest/webservices/user/UserController.java
+++ src/main/java/org/entropy/rest/webservices/user/UserController.java
@@ -6,12 +6,16 @@ import javax.validation.Valid;
 
 import org.entropy.rest.webservices.user.exception.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class UserController {
@@ -27,11 +31,16 @@ public class UserController {
 	
 	//getOneUser
 	@GetMapping(path="/users/{id}")
-	public User getOneUser(@PathVariable Integer id) {
+	public EntityModel<User> getOneUser(@PathVariable Integer id) {
 		User user = userDAO.findOne(id);
 		if (user==null)
 			throw new UserNotFoundException("User with id: "+id+" not present");
-		return user;
+		EntityModel<User> resource = EntityModel.of(user);
+		
+		WebMvcLinkBuilder linkto = linkTo(methodOn(this.getClass()).getAllUser());
+		
+		resource.add(linkto.withRel("All-users"));
+		return resource;
 	}
 	
 	//saveUser

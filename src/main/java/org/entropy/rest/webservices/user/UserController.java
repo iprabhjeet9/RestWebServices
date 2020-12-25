253a976d9d3dabefd6bb6da0be76a820ced4838c
Prabh <iprabhjeet9@gmail.com>
2020-12-25 18:53:08 +0530

    Extarcted query string

--- src/main/java/org/entropy/rest/webservices/user/UserController.java
+++ src/main/java/org/entropy/rest/webservices/user/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -24,6 +25,12 @@ public class UserController {
 	UserDAO userDAO;
 	
 	//getAllUser
+	@GetMapping(path="/hello")
+	public String hello(@RequestParam(name="msg",required=false,defaultValue="world") String msg) {
+		return "Hello "+msg;
+	}
+	
+	
 	@GetMapping(path="/users")
 	public List<User> getAllUser() {
 		return userDAO.findAll();
